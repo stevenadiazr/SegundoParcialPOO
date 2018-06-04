91de10a21e30169619e3e209efee792949a35dc8
stevenadiazr <36484829+stevenadiazr@users.noreply.github.com>
2018-06-04 16:55:52 -0600

    inicio parcial

---
+++ EjercitoHumano.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundoparcialpoo;
+
+/**
+ *
+ * @author Docente
+ */
+public class EjercitoHumano implements Humanos{
+
+    @Override
+    public void atacar() {
+        System.out.println("El ejercito ha realizado un ataque");
+    }
+    
+}
---
+++ Elfos.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package segundoparcialpoo;
+
+/**
+ *
+ * @author Docente
+ */
+public interface Elfos {
+    void atacar();
+}
